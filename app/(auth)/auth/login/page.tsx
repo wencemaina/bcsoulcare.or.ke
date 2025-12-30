238669d930145605ae44a008d3fdb2c34b6f61a8
wencemaina <mainawence@gmail.com>
2025-12-30 05:33:54 +0300

    feat: Implement user authentication with registration, login, and MongoDB integration for membership tier selection.

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import {

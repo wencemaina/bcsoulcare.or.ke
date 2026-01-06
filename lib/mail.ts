800953c36057c996c4d363478b69b772e31e3bf9
wencemaina <mainawence@gmail.com>
2026-01-06 12:40:10 +0300

    feat: Add email utility for sending OTPs for password reset.

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const getAppUrl = () => {
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || "465"),
+    port: Number(process.env.SMTP_PORT) || 465,
     secure: process.env.SMTP_SECURE === "true",
     auth: {
         user: process.env.SMTP_USER,
